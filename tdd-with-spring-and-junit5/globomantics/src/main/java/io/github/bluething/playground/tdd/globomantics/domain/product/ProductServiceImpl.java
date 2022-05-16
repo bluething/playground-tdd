9e0cdeb41ed9efbc75b8ce9664ca1c0b5bb3951b
Habib Ali Machpud <habib.machpud@gmail.com>
2022-05-17 00:17:58 +0700

    Create ProductServiceImpl.java
    The bean that we need to throw away the error.
    But the test case still error!

---
+++ tdd-with-spring-and-junit5/globomantics/src/main/java/io/github/bluething/playground/tdd/globomantics/domain/product/ProductServiceImpl.java
@@ -0,0 +1,11 @@
+package io.github.bluething.playground.tdd.globomantics.domain.product;
+
+import org.springframework.stereotype.Service;
+
+@Service
+class ProductServiceImpl implements ProductService {
+    @Override
+    public Product findById(Integer id) {
+        return null;
+    }
+}
