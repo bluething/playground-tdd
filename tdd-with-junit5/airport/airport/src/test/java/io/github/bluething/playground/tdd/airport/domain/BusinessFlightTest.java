8e3bdc3f0e77512e219f0a4912c3e1f7c370b9b5
Habib Ali Machpud <habib.machpud@gmail.com>
2022-05-16 11:16:02 +0700

    Create BusinessFlightTest.java
    Add "Add passenger to an business flight" use case skeleton

---
+++ tdd-with-junit5/airport/airport/src/test/java/io/github/bluething/playground/tdd/airport/domain/BusinessFlightTest.java
@@ -0,0 +1,19 @@
+package io.github.bluething.playground.tdd.airport.domain;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+
+@DisplayName("Given there is an business flight")
+@Nested
+public class BusinessFlightTest {
+    @DisplayName("When we have a usual passenger")
+    @Nested
+    class UsualPassenger {
+        @DisplayName("Then we can't add them to an business flight")
+        @Test
+        public void addPassengerReturnFalse() {
+            
+        }
+    }
+}
