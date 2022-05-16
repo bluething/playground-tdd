b3e3e5f7eb87a5562fdd21c99430066f1b1fd4b0
Habib Ali Machpud <habib.machpud@gmail.com>
2022-05-16 23:41:38 +0700

    Fix error to run the context

--- tdd-with-spring-and-junit5/globomantics/src/test/integration/resources/data-h2.sql
+++
@@ -1,2 +0,0 @@
-INSERT INTO products(id, name, quantitiy, version) VALUES(1, "Product 1", 10, 1)
-INSERT INTO products(id, name, quantitiy, version) VALUES(2, "Product 2", 5, 2)
---
+++ tdd-with-spring-and-junit5/globomantics/src/test/integration/resources/data.sql
@@ -0,0 +1,2 @@
+INSERT INTO products(id, name, quantity, version) VALUES(1, 'Product 1', 10, 1)
+INSERT INTO products(id, name, quantity, version) VALUES(2, 'Product 2', 5, 2)
