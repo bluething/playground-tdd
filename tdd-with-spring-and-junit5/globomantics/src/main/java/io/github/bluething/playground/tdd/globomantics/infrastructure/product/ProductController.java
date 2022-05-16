970e738700f34ed181ae4c61a0749030eefad48c
Habib Ali Machpud <habib.machpud@gmail.com>
2022-05-17 00:23:36 +0700

    Update ProductController.java
    Try to call the service, but we get another error when running the test

--- tdd-with-spring-and-junit5/globomantics/src/main/java/io/github/bluething/playground/tdd/globomantics/infrastructure/product/ProductController.java
+++ tdd-with-spring-and-junit5/globomantics/src/main/java/io/github/bluething/playground/tdd/globomantics/infrastructure/product/ProductController.java
@@ -1,12 +1,15 @@
 package io.github.bluething.playground.tdd.globomantics.infrastructure.product;
 
+import io.github.bluething.playground.tdd.globomantics.domain.product.Product;
 import io.github.bluething.playground.tdd.globomantics.domain.product.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.net.URI;
+import java.net.URISyntaxException;
 
 @RestController
 public class ProductController {
@@ -19,7 +22,12 @@ public class ProductController {
     }
 
     @GetMapping("/product/{id}")
-    public ResponseEntity<?> getProduct(@PathVariable Integer id) {
-        return new ResponseEntity<>(HttpStatus.OK);
+    public ResponseEntity<?> getProduct(@PathVariable Integer id) throws URISyntaxException {
+        Product product = productService.findById(id);
+        return ResponseEntity
+                .ok()
+                .eTag(Integer.toString(product.getVersion()))
+                .location(new URI("/product/" + product.getId()))
+                .body(product);
     }
 }
