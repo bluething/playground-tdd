291c20ee470def5ac8527df10083697d163d32c8
Habib Ali Machpud <habib.machpud@gmail.com>
2022-05-17 00:10:06 +0700

    Update ProductController.java
    We need some service. Add dependency from not existing service type.

--- tdd-with-spring-and-junit5/globomantics/src/main/java/io/github/bluething/playground/tdd/globomantics/infrastructure/product/ProductController.java
+++ tdd-with-spring-and-junit5/globomantics/src/main/java/io/github/bluething/playground/tdd/globomantics/infrastructure/product/ProductController.java
@@ -1,5 +1,6 @@
 package io.github.bluething.playground.tdd.globomantics.infrastructure.product;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -8,6 +9,14 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class ProductController {
+
+    private final ProductService productService;
+
+    @Autowired
+    public ProductController(ProductService productService) {
+        this.productService = productService;
+    }
+
     @GetMapping("/product/{id}")
     public ResponseEntity<?> getProduct(@PathVariable Integer id) {
         return new ResponseEntity<>(HttpStatus.OK);
