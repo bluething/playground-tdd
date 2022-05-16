b6ec374e1f1336d39788832f22d9d7d1911c928c
Habib Ali Machpud <habib.machpud@gmail.com>
2022-05-16 10:56:15 +0700

    Add addPassenger method

--- tdd-with-junit5/airport/airport/src/main/java/io/github/bluething/playground/tdd/airport/domain/EconomyFlight.java
+++ tdd-with-junit5/airport/airport/src/main/java/io/github/bluething/playground/tdd/airport/domain/EconomyFlight.java
@@ -3,4 +3,9 @@ package io.github.bluething.playground.tdd.airport.domain;
 public class EconomyFlight implements Flight {
     public EconomyFlight(String s) {
     }
+
+    @Override
+    public boolean addPassenger(Passenger passenger) {
+        return false;
+    }
 }
--- tdd-with-junit5/airport/airport/src/main/java/io/github/bluething/playground/tdd/airport/domain/Flight.java
+++ tdd-with-junit5/airport/airport/src/main/java/io/github/bluething/playground/tdd/airport/domain/Flight.java
@@ -1,4 +1,5 @@
 package io.github.bluething.playground.tdd.airport.domain;
 
 public interface Flight {
+    boolean addPassenger(Passenger passenger);
 }
